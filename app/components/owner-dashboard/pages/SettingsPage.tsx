86896d688ae0670ed33f7c89cc51884d64e09c97
Shana Czane M. Cruzat <shanaczanecruzat@gmail.com>
2026-03-22 22:18:38 +0800

    feat: load real user profile from Supabase in settings

--- app/components/owner-dashboard/pages/SettingsPage.tsx
+++ app/components/owner-dashboard/pages/SettingsPage.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { useAppContext } from "@/app/contexts/AppContext";
 import Sidebar from "../components/Sidebar";
 import TopNavbar from "../components/TopNavbar";
-import ConfirmDialog from "../components/Confirmdialog";
+import ConfirmDialog from "../components/ConfirmDialog";
 import SuccessModal from "../components/SuccessModal";
 
 const SettingsPage: React.FC = () => {
